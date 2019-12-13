04231a2d7c987df13fce1408e19ed915bb5f6cbc
Andrew Hyder <andrew.hyder@gsa.gov>
2019-12-13 11:49:26 -0800

    Ready for demo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
 // Add your custom javascript here
 console.log("Hi from Federalist");
+
+
+
+
+
 $(document).ready( function () {
     $('#myTable').DataTable({
         searchHighlight: true,
-        "columns": [
-            { "searchable": false, "width": "10%" },
-            { "searchable": true, "width": "10%" },
-            { "searchable": false, "width": "35%" },
-            { "searchable": false, "width": "35%" },
-            null
-        ] });
+        // "columns": [
+        //     { "searchable": true, "width": "20%" },
+        //     { "searchable": true, "width": "20%" },
+        //     { "searchable": true, "width": "20%" },
+        //     { "searchable": false, "width": "20%" },
+        //     { "searchable": false, "width": "20%" },
+        // ]
+     });
 } );
+
